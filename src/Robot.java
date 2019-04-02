5b5cf0dc1ee96f689ac99fef099cb511112346d8
maxmoehl <maximilian.moehl@gmail.com>
2019-04-02 15:34:39 +0200

    coding verbessert und dokumentation ergänzt

--- src/Robot.java
+++ src/Robot.java
@@ -3,17 +3,17 @@ import java.util.ArrayList;
 class Robot implements Runnable {
 
     /**
-     * eindeutige id zum vergleichen mit anderen Robotern
+     * Eindeutige id zum vergleichen mit anderen Robotern
      */
     private final int id;
 
     /**
-     * Die Nodes die der Roboter als nächstes abfahren muss (von 0 beginnend)
+     * Die Nodes die der Roboter als naechstes abfahren muss (von 0 beginnend)
      */
     private ArrayList<Node> graph;
 
     /**
-     * Die aktuelle Node auf der der Robotter sich befindet
+     * Die aktuelle Node auf der der Roboter sich befindet
      */
     private Node currentNode;
 
@@ -23,29 +23,26 @@ class Robot implements Runnable {
     private int inventoryMaterialType;
     private int inventoryAmount;
 
-    private Router router;
-
-    private Thread thread;
-
     /**
-     * Initialisiert einen neuen Roboter an der gegebenen Node
+     * Initialisiert einen neuen Roboter an der gegebenen Node. Jeder Roboter hat einen eigenen Thread und arbeitet deswegen voellig
+     * unabhaegig. Die Threads sind {@code isDaemmon() = true} und werden terminiert wenn der main-Thread terminiert wird.
      *
-     * @param id
-     * @param start
+     * @param id    eindeutige id
+     * @param start Node an der der Roboter initalisiert wird
      */
-    Robot(int id, Node start, Router router) {
+    Robot(int id, Node start) {
         this.id = id;
         currentNode = start;
-        this.router = router;
         inventoryMaterialType = 0;
         inventoryAmount = 0;
         graph = new ArrayList<>();
-        thread = new Thread(this);
+        Thread thread = new Thread(this);
+        thread.setDaemon(true);
         thread.start();
     }
 
     /**
-     * zum überprüfen ob der Roboter noch Nodes hat die er abarbeiten muss
+     * Zum ueberpruefen ob der Roboter noch Nodes hat die er abarbeiten muss
      *
      * @return sind im graph mehr als 0 Nodes?
      */
@@ -61,12 +58,29 @@ class Robot implements Runnable {
         return graph.get(0);
     }
 
+    /**
+     * Bewegt den Roboter zur naechste Node die auf dem Graph gegeben ist, der Vorgang sperrt den Roboter für eine Sekunde
+     *
+     * @see Robot#lock
+     */
     private void move() {
         lock(1000);
         currentNode = getNextNode();
         graph.remove(0);
     }
 
+    /**
+     * Greift auf die {@link Robot#currentNode} zu und laedt ein oder aus, muss auf einer StorageNode ausgefuehrt werden
+     * <ul>
+     * <li>Wenn der Roboter Ladung in seinem Inventar hat versucht seine Waren in die Node abzuladen</li>
+     * <li>Wenn der Roboter keine Ladung hat versucht er Waren aus der Node zu laden</li>
+     * </ul>
+     * Der Vorgang dauert eine Sekunde und sperrt die {@link Robot#currentNode}
+     *
+     * @see Robot#lock
+     * @see StorageNode#loadItems
+     * @see StorageNode#unloadItems
+     */
     private void load() {
         //Lock access to this node and lock it for one second then unload/load items
         synchronized (getCurrentNode()) {
@@ -85,14 +99,23 @@ class Robot implements Runnable {
         }
     }
 
+    /**
+     * Ueberprueft ob der Graph erneuert werden soll und erneuert diesen wenn noetig
+     */
     private void updateGraph() {
         if (graph.size() == 0) {
-            graph = router.calculateRoute(getCurrentNode(), router.getNextDestination());
+            graph = Router.getRouter().calculateRoute(getCurrentNode(), Router.getRouter().getNextDestination());
         } else {
             throw new RuntimeException("Graph hat noch Elemente und kann nicht geupdated werden");
         }
     }
 
+    /**
+     * Pausiert den Thread für eine gewisse Zeit, definiert in {@code timeout} und sperrt Ressourcen falls die Methode
+     * in einem synchronized Block ausgefuehrt wird
+     *
+     * @param timeout Zeit in Millisekunden
+     */
     private void lock(long timeout) {
         try {
             Thread.sleep(timeout);
@@ -104,11 +127,26 @@ class Robot implements Runnable {
 
     @Override
     public void run() {
-        while (router.isAlive()) {
+        while (true) {
             work();
         }
     }
 
+    /**
+     * Fuehrt einen kompletten Arbeitsdurchlauf aus
+     * <ul>
+     * <li>Wenn weitere Nodes Nodes vorhanden ist bewegt sich der Roboter eine Node weiter, indem er {@link Robot#move}</li>
+     * <li>
+     * Andernfalls wird ueberprueft ob der Roboter an dieser Node umladen kann
+     * <ul>
+     * <li>Ja: Umladen mit {@link Robot#load} und einen den Graphen erneuern {@link Robot#updateGraph}</li>
+     * <li>Nein: RuntimeException werfen</li>
+     * </ul>
+     * </li>
+     * </ul>
+     *
+     * @throws RuntimeException
+     */
     private void work() {
         //TODO remove
         printStatus();
@@ -133,8 +171,8 @@ class Robot implements Runnable {
         }
     }
 
-    //TODO remove
     private void printStatus() {
+        //TODO remove
         //System.out.println("From Current Node " + getCurrentNode().getId() + " to " + (graph.size() == 0 ? "-" : graph.get(graph.size() - 1).getId()) + " with inventory: " + inventoryMaterialType + ", " + inventoryAmount);
         System.out.println("+--------+--------+--------+-----+-----+");
         System.out.println("|akt.Node|zielNode|R-Invent|Node0|Node7|");
