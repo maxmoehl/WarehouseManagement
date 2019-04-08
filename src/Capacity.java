5eba9351c9ae01920a9f778d1b1243b4d5a89f1e
maxmoehl <maximilian.moehl@gmail.com>
2019-04-08 13:57:03 +0200

    changed encoding to utf-8

--- src/Capacity.java
+++ src/Capacity.java
@@ -1,36 +1,33 @@
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTable;
+import javax.swing.*;
+import java.awt.*;
 
 public class Capacity extends JPanel {
 
-	public Capacity() {
+    public Capacity() {
 
-		super(null);
-		JFrame capacity = new JFrame("Gesamtkapazität");
-		capacity.setSize(1500, 1000);
-		capacity.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		capacity.add(this);
+        super(null);
+        JFrame capacity = new JFrame("Gesamtkapazitï¿½t");
+        capacity.setSize(1500, 1000);
+        capacity.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        capacity.add(this);
 
-		String[] columnNames = { "Regalnummer", "Materialtyp", "Auslastung", };
-		Object[][] data = { { "Snowboarding", new Integer(5), new Boolean(false) },
-				{ "John", "Doe", "Rowing", new Integer(3), new Boolean(true) },
-				{ "Sue", "Black", "Knitting", new Integer(2), new Boolean(false) },
-				{ "Jane", "White", "Speed reading", new Integer(20), new Boolean(true) },
-				{ "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) } };
+        String[] columnNames = {"Regalnummer", "Materialtyp", "Auslastung",};
+        Object[][] data = {{"Snowboarding", new Integer(5), new Boolean(false)},
+                {"John", "Doe", "Rowing", new Integer(3), new Boolean(true)},
+                {"Sue", "Black", "Knitting", new Integer(2), new Boolean(false)},
+                {"Jane", "White", "Speed reading", new Integer(20), new Boolean(true)},
+                {"Joe", "Brown", "Pool", new Integer(10), new Boolean(false)}};
 
-		final JTable table = new JTable(data, columnNames);
-		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
-		table.setFillsViewportHeight(true);
+        final JTable table = new JTable(data, columnNames);
+        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+        table.setFillsViewportHeight(true);
 
-		capacity.setVisible(true);
-	}
+        capacity.setVisible(true);
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		new Capacity();
+        new Capacity();
 
-	}
+    }
 }
