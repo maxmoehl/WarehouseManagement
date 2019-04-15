eb7c317ab40c6cfe122071ffa68dc63b4a56438b
maxmoehl <maximilian.moehl@gmail.com>
2019-04-15 10:35:43 +0200

    update algorithmus hinzugefügt damit die Daten immer auf die Sekunde aktuell sind

--- src/Capacity.java
+++ src/Capacity.java
@@ -1,7 +1,13 @@
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 class Capacity extends JFrame {
+
+    private String[][] data;
+
+    private JTable table;
 
     Capacity() {
         super("Gesamtkapazität");
@@ -11,16 +17,33 @@ class Capacity extends JFrame {
         ArrayList<StorageNode> storageNodes = Map.getMap().storageNodes;
 
         String[][] data = new String[storageNodes.size()][3];
-
         for (int i = 0; i < storageNodes.size(); i++) {
             data[i][0] = "Storage " + storageNodes.get(i).getId();
             data[i][1] = DataConnection.getDataConnection().getMaterialType(storageNodes.get(i).getMaterialType());
             data[i][2] = storageNodes.get(i).getAmount() + " / " + storageNodes.get(i).getStorageSize();
         }
-        JTable table = new JTable(data, columnNames);
+
+        table = new JTable(data, columnNames);
         JScrollPane scrollPane = new JScrollPane(table);
         getContentPane().add(scrollPane);
         pack();
         setVisible(true);
+
+        Timer updater = new Timer(true);
+        updater.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                refreshData();
+            }
+        }, 0, 1000);
+    }
+
+    private void refreshData() {
+        ArrayList<StorageNode> storageNodes = Map.getMap().storageNodes;
+        for (int i = 0; i < storageNodes.size(); i++) {
+            table.setValueAt("Storage " + storageNodes.get(i).getId(), i, 0);
+            table.setValueAt(DataConnection.getDataConnection().getMaterialType(storageNodes.get(i).getMaterialType()), i, 1);
+            table.setValueAt(storageNodes.get(i).getAmount() + " / " + storageNodes.get(i).getStorageSize(), i, 2);
+        }
     }
 }
