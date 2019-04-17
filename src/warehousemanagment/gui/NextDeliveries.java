b16afc6bf2130d84ff9bdc50b035ce4c7d6f74a9
D073625 <hackenbruchfelix@gmail.com>
2019-04-17 16:43:55 +0200

    Umbennenung von AllDelivery zu NextDeliveries und fertigstellung der Klasse

---
+++ src/warehousemanagment/gui/NextDeliveries.java
@@ -0,0 +1,66 @@
+package warehousemanagment.gui;
+
+import warehousemanagment.DataConnection;
+import warehousemanagment.Shipment;
+
+import javax.swing.*;
+import javax.xml.crypto.Data;
+import java.awt.*;
+import java.util.*;
+import java.util.Timer;
+
+public class NextDeliveries extends JFrame {
+
+    private JTable table;
+
+    private Timer update;
+
+    public NextDeliveries() {
+        super("Anstehende Lieferungen");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        String[] columnNames = {"Lieferungstyp", "Anzahl Artikel", "Spedition", "Uhrzeit"};
+
+
+        ArrayList<Shipment> shipments = DataConnection.getDataConnection().getShipments();
+
+        String[][] data = new String[shipments.size()][columnNames.length];
+
+        for (int i = 0; i < shipments.size(); i++) {
+            data[i][0] = DataConnection.getDataConnection().getMaterialType(shipments.get(i).getMaterialType());
+            data[i][1] = shipments.get(i).getAmount() + "Stück" ;
+            //TODO Speiditionszeile hinzufügen
+            data[i][3] = "Um" + shipments.get(i).getEta();
+        }
+
+        table = new JTable(data , columnNames);
+        JScrollPane scrollPane = new JScrollPane(table);
+        getContentPane().add(scrollPane);
+        pack();
+        setVisible(true);
+
+        update = new Timer(true);
+        update.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                refreshData();
+            }
+        }, 0, 1000);
+    }
+
+    private void refreshData() {
+        ArrayList<Shipment> shipments = DataConnection.getDataConnection().getShipments();
+        for (int i = 0; i <shipments.size(); i++) {
+          table.setValueAt( DataConnection.getDataConnection().getMaterialType(shipments.get(i).getMaterialType()), i,0);
+          table.setValueAt(shipments.get(i).getAmount() + "Stück" , i, 0);
+          //TODO Speditionszeile hinzufügen
+          table.setValueAt("Um" + shipments.get(i).getEta(),i, 0 );
+        }
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        update.cancel();
+        update = null;
+    }
+}
