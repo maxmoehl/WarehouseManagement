10071bb2c6cf1e3483dc318308f391f37454e52b
maxmoehl <maximilian.moehl@gmail.com>
2019-04-17 12:28:05 +0200

    erster prototyp des hauptfensters mit MenuButtons rechts und einem JScrollPanel als platzhalter für die Map links

--- src/Panel.java
+++ src/Panel.java
@@ -1,35 +1,24 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 class Panel extends JPanel {
 
     Panel() {
-        super(null);
-    }
+        super(new GridLayout(1, 2));
 
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        int width = getWidth();
-        int height = getHeight();
-        drawMap(g, width, height);
-        drawMenu(g, width, height);
-    }
+        ((GridLayout) getLayout()).setVgap(10);
 
-    private void drawMap(Graphics g, int width, int height) {
-        g.setColor(Color.GRAY);
-        g.fillRect((int) Math.round(width * 0.01), (int) Math.round(height * 0.01), (int) Math.round(width * 0.69), (int) Math.round(height * 0.98));
-        g.setColor(Color.BLACK);
-        g.fillRect((int) Math.round(width * 0.03), (int) Math.round(height * 0.1), (int) Math.round(width * 0.45), (int) Math.round(height * 0.86));
-        g.setColor(Color.WHITE);
-        g.fillRect((int) Math.round(width * 0.04), (int) Math.round(height * 0.11), (int) Math.round(width * 0.43), (int) Math.round(height * 0.84));
-    }
+        setBorder(new EmptyBorder(10, 10, 10, 10));
 
-    private void drawMenu(Graphics g, int width, int height) {
-        g.setColor(Color.DARK_GRAY);
-        g.fillRect((int) Math.round(width * 0.72), (int) Math.round(height * 0.03), (int) Math.round(width * 0.26), (int) Math.round(height * 0.3));
-        g.fillRect((int) Math.round(width * 0.72), (int) Math.round(height * 0.35), (int) Math.round(width * 0.26), (int) Math.round(height * 0.3));
-        g.fillRect((int) Math.round(width * 0.72), (int) Math.round(height * 0.67), (int) Math.round(width * 0.26), (int) Math.round(height * 0.3));
-    }
+        JScrollPane mapContainer = new JScrollPane();
+        add(mapContainer);
 
+        JPanel buttonContainer = new JPanel(new GridLayout(3, 1));
+        ((GridLayout) buttonContainer.getLayout()).setVgap(10);
+        buttonContainer.add(new MenuButton("Capacity"));
+        buttonContainer.add(new MenuButton("Shipments"));
+        buttonContainer.add(new MenuButton("Low Space"));
+        add(buttonContainer);
+    }
 }
