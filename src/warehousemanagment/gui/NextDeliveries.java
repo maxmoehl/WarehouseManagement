700e6e9ee991af25bb58121c9475725144a143f3
D073625 <hackenbruchfelix@gmail.com>
2019-04-19 18:06:06 +0200

    doku ergaenzt und Supplier Spalte hinzugefuegt

--- src/warehousemanagment/gui/NextDeliveries.java
+++ src/warehousemanagment/gui/NextDeliveries.java
@@ -8,6 +8,10 @@ import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * Dieses Frame zeigt alle anstehende Lieferungen {@Link warehousemanagment.Shipment} an und wird, solange es offen ist, sekuendlich aktualisiert
+ */
+
 public class NextDeliveries extends JFrame {
 
     private JTable table;
@@ -27,7 +31,7 @@ public class NextDeliveries extends JFrame {
         for (int i = 0; i < shipments.size(); i++) {
             data[i][0] = DataConnection.getDataConnection().getMaterialType(shipments.get(i).getMaterialType());
             data[i][1] = shipments.get(i).getAmount() + "Stück";
-            //TODO Speiditionszeile hinzufügen
+            data[i][2] = shipments.get(i).getSupplier();
             data[i][3] = "Um" + shipments.get(i).getEta();
         }
 
@@ -50,9 +54,9 @@ public class NextDeliveries extends JFrame {
         ArrayList<Shipment> shipments = DataConnection.getDataConnection().getShipments();
         for (int i = 0; i < shipments.size(); i++) {
             table.setValueAt(DataConnection.getDataConnection().getMaterialType(shipments.get(i).getMaterialType()), i, 0);
-            table.setValueAt(shipments.get(i).getAmount() + "Stück", i, 0);
-            //TODO Speditionszeile hinzufügen
-            table.setValueAt("Um" + shipments.get(i).getEta(), i, 0);
+            table.setValueAt(shipments.get(i).getAmount() + "Stück", i, 1);
+            table.setValueAt(shipments.get(i).getSupplier(), i, 2);
+            table.setValueAt("Um" + shipments.get(i).getEta(), i, 3);
         }
     }
 
